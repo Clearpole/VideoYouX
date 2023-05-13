0f48f2f80dc6551e2b21e98055f6536a160adbff
Clearpole <clearpole@qq.com>
2023-05-13 12:45:57 +0800

    引导界面构建完毕
    Signed-off-by: Clearpole <clearpole@qq.com>

--- app/src/main/java/com/clearpole/videoyoux/compose/ui/GuideActivity.kt
+++ app/src/main/java/com/clearpole/videoyoux/compose/ui/GuideActivity.kt
@@ -44,9 +44,11 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.blankj.utilcode.util.TimeUtils
+import com.clearpole.videoyoux.MainActivity
 import com.clearpole.videoyoux.Permissions
 import com.clearpole.videoyoux.compose.ui.theme.VideoYouXTheme
 import com.clearpole.videoyoux.utils.ReadMediaStore
+import com.drake.serialize.intent.openActivity
 import com.hjq.permissions.OnPermissionCallback
 import com.hjq.permissions.XXPermissions
 import kotlinx.coroutines.CoroutineScope
@@ -92,7 +94,7 @@ class GuideActivity : ComponentActivity() {
                                 Typewriter(
                                     fixedText = "",
                                     animatedText = guideTextMore.value,
-                                    Orientation.Horizontal,10,1
+                                    Orientation.Horizontal, 10, 1
                                 )
                             }
 
@@ -108,7 +110,7 @@ class GuideActivity : ComponentActivity() {
                                     readOnly = true
                                 )
                                 Spacer(modifier = Modifier.width(15.dp))
-                                AnimatedContent(targetState =TimeUtils.getNowMills(), label = "") {
+                                AnimatedContent(targetState = TimeUtils.getNowMills(), label = "") {
                                     IconButton(
                                         onClick = {
                                             when (step.value) {
@@ -117,55 +119,77 @@ class GuideActivity : ComponentActivity() {
                                                     message.value = "我准备好了！"
                                                     step.value = 2
                                                 }
-                                                2-> {
+
+                                                2 -> {
                                                     guideText.value = "首先，\n我需要一些权限..."
                                                     message.value = "我自愿授予你这些权限"
-                                                    guideTextMore.value = "1. 存储权限：用于存储数据\n2. 访问权限：用于访问媒体库"
+                                                    guideTextMore.value =
+                                                        "1. 存储权限：用于存储数据\n2. 访问权限：用于访问媒体库"
                                                     step.value = 3
                                                 }
-                                                3-> {
+
+                                                3 -> {
                                                     guideText.value = "请允许"
-                                                    guideTextMore.value = "1. 存储权限：用于存储数据\n2. 访问权限：用于访问媒体库"
-                                                    XXPermissions.with(this@GuideActivity).permission(Permissions.PERMISSIONS)
+                                                    guideTextMore.value =
+                                                        "1. 存储权限：用于存储数据\n2. 访问权限：用于访问媒体库"
+                                                    XXPermissions.with(this@GuideActivity)
+                                                        .permission(Permissions.PERMISSIONS)
                                                         .request(object : OnPermissionCallback {
                                                             override fun onGranted(
-                                                                permissions: MutableList<String>, allGranted: Boolean
+                                                                permissions: MutableList<String>,
+                                                                allGranted: Boolean
                                                             ) {
                                                                 if (!allGranted) {
-                                                                    guideText.value = "好吧...\n您有了部分权限"
-                                                                    guideTextMore.value = "是这样的，您没能获得全部权限\n不过应该不会影响使用（\n 现在您可以进入下一步操作了！"
-                                                                    message.value = "好的，下一步是什么呢？"
+                                                                    guideText.value =
+                                                                        "好吧...\n您有了部分权限"
+                                                                    guideTextMore.value =
+                                                                        "是这样的，您没能获得全部权限\n不过应该不会影响使用（\n 现在您可以进入下一步操作了！"
+                                                                    message.value =
+                                                                        "好的，下一步是什么呢？"
                                                                     step.value = 4
                                                                     return
                                                                 }
-                                                                guideText.value = "好耶！\n您有了全部权限"
-                                                                guideTextMore.value = "谢谢，现在您可以进入下一步操作了！"
-                                                                message.value = "好的，下一步是什么呢？"
+                                                                guideText.value =
+                                                                    "好耶！\n您有了全部权限"
+                                                                guideTextMore.value =
+                                                                    "谢谢，现在您可以进入下一步操作了！"
+                                                                message.value =
+                                                                    "好的，下一步是什么呢？"
                                                                 step.value = 4
                                                             }
 
                                                             override fun onDenied(
-                                                                permissions: MutableList<String>, doNotAskAgain: Boolean
+                                                                permissions: MutableList<String>,
+                                                                doNotAskAgain: Boolean
                                                             ) {
                                                                 if (doNotAskAgain) {
-                                                                    guideText.value = "悲伤的...\n您拒绝了申请"
-                                                                    guideTextMore.value = "是这样的，现在您有三个选择\n1. 尝试再次获取权限\n2. 退出Vyx且不再使用\n3. 自行进入设置开启权限"
-                                                                    message.value = "尝试再次获取权限"
+                                                                    guideText.value =
+                                                                        "悲伤的...\n您拒绝了申请"
+                                                                    guideTextMore.value =
+                                                                        "是这样的，现在您有三个选择\n1. 尝试再次获取权限\n2. 退出Vyx且不再使用\n3. 自行进入设置开启权限"
+                                                                    message.value =
+                                                                        "尝试再次获取权限"
                                                                 } else {
-                                                                    guideText.value = "悲伤的...\n获取失败了"
-                                                                    guideTextMore.value = "是这样的，现在您只有两个选择\n1. 退出Vyx且不再使用\n2. 自行进入设置开启权限"
-                                                                    message.value = "尝试再次获取权限"
+                                                                    guideText.value =
+                                                                        "悲伤的...\n获取失败了"
+                                                                    guideTextMore.value =
+                                                                        "是这样的，现在您只有两个选择\n1. 退出Vyx且不再使用\n2. 自行进入设置开启权限"
+                                                                    message.value =
+                                                                        "尝试再次获取权限"
                                                                 }
                                                             }
                                                         })
                                                 }
-                                                4->{
+
+                                                4 -> {
                                                     guideText.value = "现在\n我要初始化了！"
-                                                    guideTextMore.value = "是否开始初始化？\n简单来说就是首次读取本地所有视频。"
+                                                    guideTextMore.value =
+                                                        "是否开始初始化？\n简单来说就是首次读取本地所有视频。"
                                                     message.value = "我要开始初始化"
                                                     step.value = 5
                                                 }
-                                                5->{
+
+                                                5 -> {
                                                     guideText.value = "初始化中"
                                                     guideTextMore.value = "请耐心等待初始化完毕。"
                                                     message.value = ""
@@ -173,18 +197,32 @@ class GuideActivity : ComponentActivity() {
                                                         ReadMediaStore.writeData(contentResolver)
                                                         delay(1500)
                                                         guideText.value = "检查资源"
-                                                        guideTextMore.value = "正在校验资源完整性..."
-                                                        while (true){
+                                                        guideTextMore.value =
+                                                            "正在校验资源完整性..."
+                                                        while (true) {
                                                             delay(500)
-                                                            if (ReadMediaStore.readFolder().isNotEmpty()){
+                                                            if (ReadMediaStore.readFolder()
+                                                                    .isNotEmpty()
+                                                            ) {
                                                                 guideText.value = "一切准备就绪"
-                                                                guideTextMore.value = "现在，开启你的Vyx之旅吧！\n祝您有个美好的使用体验。"
+                                                                guideTextMore.value =
+                                                                    "现在，开启你的Vyx之旅吧！\n祝您有个美好的使用体验。"
                                                                 message.value = "让我进去！"
                                                                 step.value = 6
                                                                 break
                                                             }
                                                         }
                                                     }
+                                                }
+
+                                                6 -> {
+                                                    val sharedPreferences =
+                                                        getSharedPreferences("values", MODE_PRIVATE)
+                                                    val editor = sharedPreferences.edit()
+                                                    editor.putBoolean("first-enter", false)
+                                                    editor.apply()
+                                                    openActivity<MainActivity>()
+                                                    finish()
                                                 }
                                             }
                                         },
@@ -246,7 +284,7 @@ class GuideActivity : ComponentActivity() {
                         letterSpacing = -(1.6).sp,
                         lineHeight = 52.sp,
                     ),
-                    modifier = Modifier.padding(top = 80.dp, start = 30.dp,end = 25.dp)
+                    modifier = Modifier.padding(top = 80.dp, start = 30.dp, end = 25.dp)
                 )
             }
 
