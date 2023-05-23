63a2dc74477a7808a8f5569756ce2e0b8f123cf9
Clearpole <clearpole@qq.com>
2023-05-23 21:44:37 +0800

    修复引导页取色异常
    Signed-off-by: Clearpole <clearpole@qq.com>

--- app/src/main/java/com/clearpole/videoyoux/compose/ui/theme/Theme.kt
+++ app/src/main/java/com/clearpole/videoyoux/compose/ui/theme/Theme.kt
@@ -1,21 +1,49 @@
 package com.clearpole.videoyoux.compose.ui.theme
 
 import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import com.clearpole.videoyoux.utils.System
 import com.gyf.immersionbar.BarHide
 import com.gyf.immersionbar.ImmersionBar
 
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
+)
 
 @Composable
 fun VideoYouXTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
     hideBar: Boolean,
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
 
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
@@ -29,6 +57,7 @@ fun VideoYouXTheme(
     }
 
     MaterialTheme(
+        colorScheme = colorScheme,
         typography = Typography,
         content = content
     )
