db2a9086e301ef1ae305c7df481f4d11a981245f
稻光千叶 <clearpole@qq.com>
2023-07-16 23:42:43 +0800

    重构引导页[暂未适配横屏]
    问：如何体验？
    答：清除全部数据并竖屏

--- app/src/main/java/com/clearpole/videoyoux/GuideActivity.kt
+++ app/src/main/java/com/clearpole/videoyoux/GuideActivity.kt
@@ -1,5 +1,6 @@
 package com.clearpole.videoyoux
 
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.os.Bundle
 import android.view.View
@@ -7,25 +8,29 @@ import android.view.animation.AccelerateInterpolator
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.view.animation.Animation.AnimationListener
+import android.view.animation.CycleInterpolator
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
 import com.clearpole.videoyoux._adapter.ViewPagerAdapter
 import com.clearpole.videoyoux._assembly.FixedSpeedScroller
 import com.clearpole.videoyoux._models.GuidePermissionModel
+import com.clearpole.videoyoux._utils.ReadMediaStore
 import com.clearpole.videoyoux.databinding.ActivityGuideBinding
 import com.clearpole.videoyoux.databinding.ActivityGuideLandBinding
 import com.drake.brv.utils.linear
 import com.drake.brv.utils.setup
+import com.drake.serialize.intent.openActivity
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.progressindicator.CircularProgressIndicator
 import com.google.android.material.textview.MaterialTextView
+import com.hjq.permissions.XXPermissions
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.lang.reflect.Field
-
 
 class GuideActivity : BaseActivity<ActivityGuideBinding, ActivityGuideLandBinding>(
     isHideStatus = false,
@@ -66,16 +71,67 @@ class GuideActivity : BaseActivity<ActivityGuideBinding, ActivityGuideLandBindin
             }
             it[1].apply {
                 findViewById<MaterialTextView>(R.id.guide_permission_back).setOnClickListener {
+                    viewPagerAnimation(view, false)
                     binding.guidePagerView.setCurrentItem(0, true)
                 }
-                findViewById<RecyclerView>(R.id.guide_permission_list).linear().setup {
+                val recyclerView = findViewById<RecyclerView>(R.id.guide_permission_list)
+                recyclerView.linear().setup {
                     addType<GuidePermissionModel> { R.layout.guide_permission_list }
                 }.models = mutableListOf<Any?>().apply {
-                    add(GuidePermissionModel(Permissions.storage,
-                        context.getString(R.string.read_video_permission),
-                        context.getString(R.string.permission_use_storage),
-                        AppCompatResources.getDrawable(this@GuideActivity,R.drawable.outline_video_file_24)
-                    ))
+                    add(
+                        GuidePermissionModel(
+                            Permissions.storage,
+                            context.getString(R.string.read_video_permission),
+                            context.getString(R.string.permission_use_storage),
+                            AppCompatResources.getDrawable(
+                                this@GuideActivity,
+                                R.drawable.outline_video_file_24
+                            )
+                        )
+                    )
+                }
+                findViewById<MaterialButton>(R.id.guide_permission_next).setOnClickListener {
+                    if (XXPermissions.isGranted(this@GuideActivity, Permissions.storage)) {
+                        viewPagerAnimation(view, false)
+                        binding.guidePagerView.setCurrentItem(2, true)
+                    } else {
+                        val anim =
+                            ObjectAnimator.ofFloat(recyclerView, View.TRANSLATION_X.name, 0f, 8f)
+                        anim.setDuration(300)
+                        anim.interpolator = CycleInterpolator(4f)
+                        anim.start()
+                    }
+                }
+            }
+            it[2].apply {
+                findViewById<MaterialTextView>(R.id.guide_data_store_back).setOnClickListener {
+                    viewPagerAnimation(view, false)
+                    binding.guidePagerView.setCurrentItem(1, true)
+                }
+                findViewById<MaterialButton>(R.id.guide_data_store_next).setOnClickListener {
+                    if ((it as MaterialButton).text == "开始扫描") {
+                        val progressView =
+                            findViewById<CircularProgressIndicator>(R.id.guide_data_store_progress)
+                        progressView.visibility = View.VISIBLE
+                        it.text = "正在扫描中"
+                        CoroutineScope(Dispatchers.IO).launch {
+                            ReadMediaStore.writeData(contentResolver).apply {
+                                delay(1000)
+                                withContext(Dispatchers.Main) {
+                                    progressView.visibility = View.GONE
+                                    it.text = "进入主页"
+                                }
+                            }
+                        }
+                    } else if (it.text == "进入主页") {
+                        val sharedPreferences =
+                            getSharedPreferences("values", MODE_PRIVATE)
+                        val editor = sharedPreferences.edit()
+                        editor.putBoolean("first-enter", false)
+                        editor.apply()
+                        openActivity<MainActivity>()
+                        finish()
+                    }
                 }
             }
         }
@@ -83,7 +139,7 @@ class GuideActivity : BaseActivity<ActivityGuideBinding, ActivityGuideLandBindin
 
     private fun viewPagerAnimation(view: View, back: Boolean) {
         AlphaAnimation(1f, 0f).apply {
-            duration = 150
+            duration = if (back) 10 else 150
             view.startAnimation(this)
         }.setAnimationListener(object : AnimationListener {
             override fun onAnimationStart(p0: Animation?) {}
@@ -108,7 +164,7 @@ class GuideActivity : BaseActivity<ActivityGuideBinding, ActivityGuideLandBindin
     private fun controlViewPagerSpeed(
         context: Context?,
         viewpager: ViewPager?,
-        DurationSwitch: Int
+        duration: Int
     ) {
         try {
             val mField: Field = ViewPager::class.java.getDeclaredField("mScroller")
@@ -117,7 +173,7 @@ class GuideActivity : BaseActivity<ActivityGuideBinding, ActivityGuideLandBindin
                 context,
                 AccelerateInterpolator()
             )
-            mScroller!!.setmDuration(DurationSwitch)
+            mScroller!!.setmDuration(duration)
             mField.set(viewpager, mScroller)
         } catch (e: Exception) {
             e.printStackTrace()
--- app/src/main/java/com/clearpole/videoyoux/_models/GuidePermissionModel.kt
+++ app/src/main/java/com/clearpole/videoyoux/_models/GuidePermissionModel.kt
@@ -1,20 +1,62 @@
 package com.clearpole.videoyoux._models
 
 import android.graphics.drawable.Drawable
+import androidx.appcompat.content.res.AppCompatResources
+import com.clearpole.videoyoux.R
 import com.clearpole.videoyoux.databinding.GuidePermissionListBinding
 import com.drake.brv.BindingAdapter
 import com.drake.brv.item.ItemBind
+import com.drake.tooltip.toast
+import com.hjq.permissions.OnPermissionCallback
+import com.hjq.permissions.XXPermissions
 
 class GuidePermissionModel(
     private val permission: String,
     private val title: String,
     private val info: String,
-    private val icon:Drawable?
+    private val icon: Drawable?
 ) : ItemBind {
     override fun onBind(holder: BindingAdapter.BindingViewHolder) {
         val binding = holder.getBinding<GuidePermissionListBinding>()
         binding.img.setImageDrawable(icon)
         binding.title.text = title
         binding.info.text = info
+        binding.icon.setImageDrawable(
+            if (XXPermissions.isGranted(holder.context, permission)) {
+                AppCompatResources.getDrawable(
+                    holder.context,
+                    R.drawable.round_done_24
+                )
+            } else {
+                AppCompatResources.getDrawable(holder.context, R.drawable.round_close_24)
+            }
+        )
+        binding.root.setOnClickListener {
+            XXPermissions.with(holder.context)
+                .permission(permission)
+                .request(object : OnPermissionCallback {
+                    override fun onGranted(
+                        permissions: MutableList<String>,
+                        allGranted: Boolean
+                    ) {
+                        if (allGranted) {
+                            binding.icon.setImageDrawable(
+                                AppCompatResources.getDrawable(
+                                    holder.context,
+                                    R.drawable.round_done_24
+                                )
+                            )
+                        }
+                    }
+
+                    override fun onDenied(
+                        permissions: MutableList<String>,
+                        doNotAskAgain: Boolean
+                    ) {
+                        toast("请自行开启权限：${permission.replace("android.permission.", "")}")
+                        XXPermissions.startPermissionActivity(holder.context)
+                    }
+                })
+        }
     }
 }
