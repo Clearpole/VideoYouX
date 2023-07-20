5756bbe8311b6b4b305c179ec3ca9f141b8fe9e3
Light_summer <93428659+Lightsummer233@users.noreply.github.com>
2023-07-20 19:07:39 +0800

    Java 20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,12 +100,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_19
-        targetCompatibility = JavaVersion.VERSION_19
+        sourceCompatibility = JavaVersion.VERSION_20
+        targetCompatibility = JavaVersion.VERSION_20
     }
 
     kotlinOptions {
-        jvmTarget = "19"
+        jvmTarget = "20"
     }
 
     buildFeatures {
