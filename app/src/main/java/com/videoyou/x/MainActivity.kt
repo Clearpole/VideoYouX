e914c1f6174c4af0f08cc0b05d6ca82d7b77e721
稻光千叶 <clearpole@qq.com>
2023-08-01 17:32:05 +0800

    first commit

---
+++ app/src/main/java/com/videoyou/x/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.videoyou.x
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
