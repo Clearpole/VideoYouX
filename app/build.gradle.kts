1cbcc45c39d81c263fd52eb3506f3f6ec2366ee3
Light_summer <lightsummer233@gmail.com>
2023-08-26 23:01:14 +0800

    添加 suppressKotlinVersionCompatibilityCheck

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -117,6 +117,9 @@ android {
 
     kotlinOptions {
         jvmTarget = "17"
+        freeCompilerArgs += listOf(
+            "-P", "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=true"
+        )
     }
 
     buildFeatures {
