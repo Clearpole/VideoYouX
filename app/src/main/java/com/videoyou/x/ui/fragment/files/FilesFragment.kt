daacd8ca67439848928b2d30ac4dbb4f06198905
Clearpole <clearpole@qq.com>
2024-01-12 19:22:55 +0800

    内部测试
    Signed-off-by: Clearpole <clearpole@qq.com>

--- app/src/main/java/com/videoyou/x/ui/fragment/files/FilesFragment.kt
+++ app/src/main/java/com/videoyou/x/ui/fragment/files/FilesFragment.kt
@@ -1,12 +1,13 @@
 package com.videoyou.x.ui.fragment.files
 
+import android.widget.Toast
 import com.videoyou.x._utils.base.BaseFragment
 import com.videoyou.x.databinding.FragmentMainFilesBinding
 import com.videoyou.x.ui.fragment.files.model.FilesViewModel
 
 class FilesFragment:BaseFragment<FilesViewModel,FragmentMainFilesBinding>() {
     override fun onViewCreate() {
-
+        Toast.makeText(requireContext(), "没闪退？", Toast.LENGTH_SHORT).show()
     }
 
     override fun getViewBinding(): FragmentMainFilesBinding {
