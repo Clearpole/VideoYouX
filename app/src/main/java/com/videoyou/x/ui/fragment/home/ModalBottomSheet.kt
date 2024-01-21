30e8f5a264834d9849d087e6c23f062f54be7e69
Clearpole <clearpole@qq.com>
2024-01-21 21:05:06 +0800

    修复文件体积显示错误 修复子级文件错误显示在父级文件夹下的问题 文件夹内视频可播放

--- app/src/main/java/com/videoyou/x/ui/fragment/home/ModalBottomSheet.kt
+++ app/src/main/java/com/videoyou/x/ui/fragment/home/ModalBottomSheet.kt
@@ -1,21 +1,21 @@
 package com.videoyou.x.ui.fragment.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
+import androidx.media3.common.MediaItem
 import com.drake.brv.utils.linear
 import com.drake.brv.utils.setup
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.videoyou.x.R
 import com.videoyou.x.databinding.FoldersSheetBinding
+import com.videoyou.x.player.Play
 import com.videoyou.x.storage.AndroidMediaStore
 import com.videoyou.x.ui.fragment.home.model.VideosModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -45,12 +45,12 @@ class ModalBottomSheet(private val folderPath: String) : BottomSheetDialogFragme
     private fun models(): MutableList<Any> =
         mutableListOf<Any>().apply {
             val kv = AndroidMediaStore.readFoldersVideosData()
+            val arrayList = arrayListOf<MediaItem>()
             kv.allKeys()!!.forEach {
-                if (it.contains(folderPath)) {
-                    if (it.split("\u001A")[0]==folderPath) {
-                        val data = kv.decodeString(it)!!.split("\u001A")
-                        add(VideosModel(data[0], data[3], data[4]))
-                    }
+                if (it.contains(folderPath) && it.split("\u001A")[0] == folderPath) {
+                    val data = kv.decodeString(it)!!.split("\u001A")
+                    arrayList.add(MediaItem.fromUri(data[1].toUri()))
+                    add(VideosModel(data[0], data[3], data[4],data[1],arrayList))
                 }
             }
         }
--- app/src/main/java/com/videoyou/x/ui/fragment/home/model/VideosModel.kt
+++ app/src/main/java/com/videoyou/x/ui/fragment/home/model/VideosModel.kt
@@ -1,17 +1,23 @@
 package com.videoyou.x.ui.fragment.home.model
 
+import androidx.core.net.toUri
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
 import com.drake.brv.BindingAdapter
 import com.drake.brv.item.ItemBind
+import com.drake.serialize.intent.openActivity
+import com.videoyou.x.PlayerActivity
 import com.videoyou.x.databinding.ItemVideosItemBinding
+import com.videoyou.x.player.Play
 
 class VideosModel(
     private val videoPath: String,
     private val mVideoTitle: String,
-    private val videoSize: String
+    private val videoSize: String,
+    private val videoUri: String,
+    private val playList: ArrayList<androidx.media3.common.MediaItem>
 ) : ItemBind {
     override fun onBind(vh: BindingAdapter.BindingViewHolder) {
         vh.getBinding<ItemVideosItemBinding>().apply {
@@ -27,6 +33,14 @@ class VideosModel(
                 videoSubTitle.text = String.format("%.2f", (size / 1024F)) + " GiB"
             } else {
                 videoSubTitle.text = String.format("%.2f", size) + " MiB"
+            }
+            clickRoot.setOnClickListener {
+                Play.list = playList
+                Play.position = vh.layoutPosition
+                vh.context.openActivity<PlayerActivity>(
+                    "path" to videoPath,
+                    "uri" to videoUri.toUri()
+                )
             }
         }
     }
